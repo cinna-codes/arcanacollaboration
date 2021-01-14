637b34f29e7366967ad6988be228d8ba7e35568f
cinna-codes <somethingfellipickeditup@gmail.com>
2021-01-14 18:52:20 -0500

    added basic name validations to . added  private method to .

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,11 @@ class UsersController < ApplicationController
 
     def show
     end
+
+    private
+    
+    def user_params
+        params.require(:user).permit(:name, :password, :password_confirmation)
+    end
     
 end
