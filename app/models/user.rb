62a2182cb6e63d4935793611d29e4709336c6a60
cinna-codes <somethingfellipickeditup@gmail.com>
2021-01-14 18:55:37 -0500

    added basic email validations to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     validates :name, presence: true
     validates :name, uniqueness: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
     
 end
