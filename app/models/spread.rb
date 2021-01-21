96e848037a21eba385bd09ec3a3b260f6b32d5e5
cinna-codes <somethingfellipickeditup@gmail.com>
2021-01-21 14:34:11 -0500

    added 'has_many :posts' relationship to spread.rb & post.rb

--- app/models/spread.rb
+++ app/models/spread.rb
@@ -5,4 +5,6 @@ class Spread < ApplicationRecord
     validates :desc, presence: true
     validates :num_of_cards, presence: true
 
+    has_many :posts
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
     validates :name, uniqueness: true
     validates :email, presence: true
     validates :email, uniqueness: true
+
+    has_many :posts
     
 end
