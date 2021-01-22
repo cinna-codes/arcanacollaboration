c031605e85d149a78e7bbb3e67ba3f1fc012e5f4
cinna-codes <somethingfellipickeditup@gmail.com>
2021-01-21 23:58:48 -0500

    added belongs_to post & card in reading model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,11 @@
 class Post < ApplicationRecord
     belongs_to :user
     belongs_to :spread
+    has_many :readings
+    has_many :cards, through: :readings
 
     validates :title, uniqueness: {
         scope: %i[user_id title],
-        message: 'titles must be unique per user'
+        message: 'post titles must be unique per user'
     }
 end
