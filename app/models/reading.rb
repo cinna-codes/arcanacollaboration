ad082eeeb7f8ff6457c800066bc7b99449ca19ab
cinna-codes <somethingfellipickeditup@gmail.com>
2021-01-22 03:12:44 -0500

    removed 'upright' column from create_readings migration & matching validation. changed reading's 'belongs_to :post' relationship to 'belongs_to :user

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -1,8 +1,7 @@
 class Reading < ApplicationRecord
-    belongs_to :post
+    belongs_to :user
     belongs_to :card
 
-    validates :upright, presence: true
     validates :desc, presence: true
     validates :post_id, presence: true
     validates :spread_id, presence: true
--- db/migrate/20210121204911_create_readings.rb
+++ db/migrate/20210121204911_create_readings.rb
@@ -1,7 +1,6 @@
 class CreateReadings < ActiveRecord::Migration[6.0]
   def change
     create_table :readings do |t|
-      t.boolean :upright, default: true
       t.string :desc
       t.belongs_to :post, null: false, foreign_key: true
       t.belongs_to :card, null: false, foreign_key: true
