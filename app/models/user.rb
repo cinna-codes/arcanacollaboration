ccd3f8aa0fb78d10485e37826cbc1f2f27c60747
cinna-codes <somethingfellipickeditup@gmail.com>
2021-01-22 04:06:47 -0500

    changed user table 'name' column to 'username'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :name, presence: true
-    validates :name, uniqueness: true
+    validates :username, presence: true
+    validates :username, uniqueness: true
     validates :email, presence: true
     validates :email, uniqueness: true
 
