ce74afa493b416a3505b8543845b6dc9b7838ef3
cinna-codes <somethingfellipickeditup@gmail.com>
2021-01-21 23:10:09 -0500

    added reading model + basic validations

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -1,3 +1,6 @@
 class Reading < ApplicationRecord
-    
+    validates :upright, presence: true
+    validates :desc, presence: true
+    validates :post_id, presence: true
+    validates :spread_id, presence: true
 end
