f762f8d9741d9d34193b304010225ae7f4499480
cinna-codes <somethingfellipickeditup@gmail.com>
2021-01-26 15:00:16 -0500

    corrected readings to belong to user instead of post

--- db/migrate/20210121204911_create_readings.rb
+++ db/migrate/20210121204911_create_readings.rb
@@ -2,7 +2,7 @@ class CreateReadings < ActiveRecord::Migration[6.0]
   def change
     create_table :readings do |t|
       t.string :desc
-      t.belongs_to :post, null: false, foreign_key: true
+      t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :card, null: false, foreign_key: true
     end
   end
