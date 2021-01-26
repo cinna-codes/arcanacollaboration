2758bb1a2c7084483b95eb33fbee888d63bcaa13
cinna-codes <somethingfellipickeditup@gmail.com>
2021-01-26 18:36:45 -0500

    fixed syntax error in routes.rb wherein 'resources' were instead listed as 'resource'

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,13 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'users#new', as: 'signup'
 
-  resource :users, except: [:destroy, :edit, :update, :new] do
-    resource :readings, only: [:index, :show]
+  resources :users, except: [:destroy, :edit, :update, :new] do
+    resources :readings, only: [:index, :show]
   end
 
-  resource :spreads, only: [:index, :show] do
-    resource :readings, only: [:index, :show, :new]
+  resources :spreads, only: [:index, :show] do
+    resources :readings, only: [:index, :show, :new]
   end
 
-  resource :readings
+  resources :readings
 end
