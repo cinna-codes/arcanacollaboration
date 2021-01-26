6c6b0183be43d988578408612e9163e7cc88cc9b
cinna-codes <somethingfellipickeditup@gmail.com>
2021-01-26 18:37:43 -0500

    fixed syntax error in routes.rb wherein 'resources' were instead listed as 'resource'

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'static#home'
   get '/login', to: 'sessions#new', as: 'login'
-  post '/login', to: 'sessions#create', as: 'login'
+  post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy', as: 'logout'
 
   get '/signup', to: 'users#new', as: 'signup'
