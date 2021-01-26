4f9e90d81efb7d18e565559f77731a894a08b4b2
cinna-codes <somethingfellipickeditup@gmail.com>
2021-01-26 18:32:55 -0500

    set sessions routes to custom path names

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'static#home'
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create', as: 'login'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
 
-  get '/signup', to: 'users#new'
+  get '/signup', to: 'users#new', as: 'signup'
 
   resource :users, except: [:destroy, :edit, :update, :new] do
     resource :readings, only: [:index, :show]
