890a40f9f561fbc0ac086cae9d8d1ecf35167d3f
cinna-codes <somethingfellipickeditup@gmail.com>
2021-01-26 17:24:59 -0500

    added basic variables to routes in spreads_controller.rb

--- app/controllers/spreads_controller.rb
+++ app/controllers/spreads_controller.rb
@@ -1,9 +1,9 @@
 class SpreadsController < ApplicationController
     def index
-
+        @spreads = Spread.all
     end
 
     def show
-        
+        @spread = Spread.find_by(id: params[:id])
     end
 end
