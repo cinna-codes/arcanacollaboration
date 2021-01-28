8ea119304c1a0627c15e7d3206bd34f90b59ad13
cinna-codes <somethingfellipickeditup@gmail.com>
2021-01-27 23:44:48 -0500

    edited spread_id_field reading helper to account for automatically assigning the spread_id when visiting spreads/:id/readings/new

--- app/helpers/readings_helper.rb
+++ app/helpers/readings_helper.rb
@@ -1,6 +1,8 @@
 module ReadingsHelper
     def spread_id_field(reading)
-        if reading.spread.nil?
+        if params[:spread_id]
+          hidden_field_tag "reading[spread_id]", params[:spread_id]
+        elsif reading.spread.nil?
           select_tag "reading[spread_id]", options_from_collection_for_select(Spread.all, :id, :title)
         else
           hidden_field_tag "reading[spread_id]", reading.spread_id
