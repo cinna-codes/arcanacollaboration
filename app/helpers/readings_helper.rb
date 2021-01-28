aa06cbfafca8925a9b6489283f0dc074bfd7be9e
cinna-codes <somethingfellipickeditup@gmail.com>
2021-01-27 23:06:18 -0500

    created readings helper method for the spread_id field on reading forms

--- app/helpers/readings_helper.rb
+++ app/helpers/readings_helper.rb
@@ -1,2 +1,9 @@
 module ReadingsHelper
+    def spread_id_field(reading)
+        if reading.spread.nil?
+          select_tag "reading[spread_id]", options_from_collection_for_select(Spread.all, :id, :title)
+        else
+          hidden_field_tag "reading[spread_id]", reading.spread_id
+        end
+    end
 end
