7380446203cf2481b97413e4a586f4e3c62ad471
cinna-codes <somethingfellipickeditup@gmail.com>
2021-01-28 02:02:05 -0500

    current_user method set to basic user for testing purposes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
     private
 
     def current_user
-        current_user ||= User.find_by(id: session[:user_id])
+        current_user = User.all.first
+        # current_user ||= User.find_by(id: session[:user_id])
     end
 
     def logged_in
