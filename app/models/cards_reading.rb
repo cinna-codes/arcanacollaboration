5e89e80134db6235c2d4f41d196eb08e2ce9443c
cinna-codes <somethingfellipickeditup@gmail.com>
2021-01-27 22:50:14 -0500

    added a validation to cards_reading wherein you cannot repeat the same card_id within one reading_id. corrected potential syntax error on num: uniqueness validator in same class.

--- app/models/cards_reading.rb
+++ app/models/cards_reading.rb
@@ -4,10 +4,16 @@ class CardsReading < ApplicationRecord
 
     validates :num, presence: true
     validates :num, uniqueness: {
-        scope: %i[reading],
+        scope: %i[reading_id],
         message: 'cards cannot share a spot in order with one another, e.g. two cards cannot be considered first in order'
     }
     validates :upright, presence: true
     validates :card_id, presence: true
     validates :reading_id, presence: true
+
+    validates :card_id, uniqueness: {
+        scope: %i[reading_id],
+        message: 'cannot draw the same card twice'
+    }
+
 end
