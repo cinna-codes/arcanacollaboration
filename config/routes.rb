29cf8c766d9ca89be65853329a73a798b2d945e6
cinna-codes <somethingfellipickeditup@gmail.com>
2021-01-28 22:40:09 -0500

    button_to logout_path now works

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'static#home'
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy', as: 'logout'
+  delete '/logout', to: 'sessions#destroy'
 
   match '/auth/github/callback', to: 'sessions#github', via: [:get, :post]
 
