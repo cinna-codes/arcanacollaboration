a1ec9cd582e25dc6f30f63ef079657ddf7894795
cinna-codes <somethingfellipickeditup@gmail.com>
2021-01-28 21:19:36 -0500

    commented out previous commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
             session[:user_id] = user.id
             redirect_to user_path(user)
         else
-            flash[:message] = "An error occurred. Please try to login again."
+            # flash[:message] = "An error occurred. Please try to login again."
             redirect_to login_path
         end
     end
