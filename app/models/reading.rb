2f688006eb47d876b7513878e3ad6fdfbeb8543a
cinna-codes <somethingfellipickeditup@gmail.com>
2021-01-29 01:09:12 -0500

    added reading scope methods for pulling records belonging to certain users or spreads, which can be chained on each other

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -9,4 +9,7 @@ class Reading < ApplicationRecord
     validates :spread_id, presence: true
 
     accepts_nested_attributes_for :cards_readings
+
+    scope :belongs_to_user, ->(user_id_x) { where("user_id = ?", user_id_x) }
+    scope :belongs_to_spread, ->(spread_id_x) { where("spread_id = ?", spread_id_x) }
 end
