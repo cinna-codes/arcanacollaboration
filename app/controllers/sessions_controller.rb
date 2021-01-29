d645a2671f29c3ea79b757ee82c72a24e7381f1c
cinna-codes <somethingfellipickeditup@gmail.com>
2021-01-28 21:18:39 -0500

    flash[:message] now set for failing to log in through omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,6 +17,7 @@ class SessionsController < ApplicationController
             session[:user_id] = user.id
             redirect_to user_path(user)
         else
+            flash[:message] = "An error occurred. Please try to login again."
             redirect_to login_path
         end
     end
