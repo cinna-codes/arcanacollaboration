c5be0e0031894ef1861a6943bf973d20fd314f2e
cinna-codes <somethingfellipickeditup@gmail.com>
2021-01-28 22:41:35 -0500

    removed validations for presence/uniqueness of :num on cards_readings, as you can not define that number through the reading new form

--- app/models/cards_reading.rb
+++ app/models/cards_reading.rb
@@ -2,11 +2,11 @@ class CardsReading < ApplicationRecord
     belongs_to :card
     belongs_to :reading
 
-    validates :num, presence: true
-    validates :num, uniqueness: {
-        scope: %i[reading_id],
-        message: 'cards cannot share a spot in order with one another, e.g. two cards cannot be considered first in order'
-    }
+    # validates :num, presence: true
+    # validates :num, uniqueness: {
+    #     scope: %i[reading_id],
+    #     message: 'cards cannot share a spot in order with one another, e.g. two cards cannot be considered first in order'
+    # }
     # validates :upright, presence: true
     validates :card_id, presence: true
     # validates :reading_id, presence: true
