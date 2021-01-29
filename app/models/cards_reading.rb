8ffc76385e8387b5ebec3a92e919c13e22879520
cinna-codes <somethingfellipickeditup@gmail.com>
2021-01-29 00:20:36 -0500

    added 'card_name', 'position', and 'card_name_and_position' instance methods to cards_reading class.

--- app/models/cards_reading.rb
+++ app/models/cards_reading.rb
@@ -23,4 +23,17 @@ class CardsReading < ApplicationRecord
     scope :is_card, ->(is_card_id) { where("card_id = ?", is_card_id) }
     # implement at least one somewhere
 
+
+    def card_name_and_position
+        "#{self.card_name}, #{self.position}"
+    end
+
+    def card_name
+        self.card.name
+    end
+
+    def position
+        self.upright ? "Upright" : "Reversed"
+    end
+
 end
